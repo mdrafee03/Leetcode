4e04db9847c4ebdc2dfdef96ad7f5497fbdf91f9
Md. Rafee <mdrafee03@gmail.com>
2022-01-10 09:16:00 +0600

    Time: 2 ms (49.60%), Space: 52.7 MB (8.32%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if(nums==null || nums.length==0) return;
         int insertPos = 0;
         for (int num: nums) {
             if (num != 0) nums[insertPos++] = num;
